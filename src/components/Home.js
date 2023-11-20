8062a7fa6d1925459a6c75b4dd8055bfaa37c264
unknown <philippe.berck70@gmail.com>
2023-11-20 17:26:08 +0100

    init

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ function Home() {
     return (
         <div className='container mt-3'>
             <h3>Accueil</h3>
+            <p>Ce site s'adresse aux personnes qui souhaitent tester leurs compétences rédactionnelles en français.</p>
             <ul>
                 <li><Link to="/contact">Contact</Link></li>
                 <li><Link to="/exerciceslist">Liste des exercices</Link></li>
