f27a7b67c020eedde902e2a6f8f7241dc0a19a3a
unknown <philippe.berck70@gmail.com>
2023-11-20 17:12:36 +0100

    Updates

---
+++ static/js/383.0b63f416.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_gopb=self.webpackChunkreact_gopb||[]).push([[383],{383:e=>{e.exports=JSON.parse('{"sentences":[{"id":1,"name":"Les enfants _ dans le bois.","options":[{"id":1,"name":"jouent"},{"id":2,"name":"joue"}],"idok":1}]}')}}]);
